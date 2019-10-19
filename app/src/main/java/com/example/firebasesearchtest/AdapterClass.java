5e01a9f7d3e816c54921e6a1d2c023b7306a2eba
KHA <gamerx6900@gmail.com>
2019-10-20 01:43:37 +0300

    copy from project(firebaseSearchTest03) 20/10/2019 Sunday 1:42 AM not tested, unfixed pic loader.

---
+++ app/src/main/java/com/example/firebasesearchtest/AdapterClass.java
@@ -0,0 +1,54 @@
+package com.example.firebasesearchtest;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+
+import java.util.ArrayList;
+
+public class AdapterClass extends RecyclerView.Adapter<AdapterClass.MyViewHolder> {
+    ArrayList<Users> list;
+
+    public AdapterClass(ArrayList<Users> list){
+        this.list = list;
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder{
+        View mView;
+        TextView name,status;
+        ImageView imageView;
+        public MyViewHolder(View itemVew){
+            super(itemVew);
+            mView = itemVew;
+            name = mView.findViewById(R.id.tvName);
+            status = mView.findViewById(R.id.tvStatus);
+            imageView = mView.findViewById(R.id.ivUsers);
+        }
+
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_layout, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AdapterClass.MyViewHolder holder, int position) {
+        holder.name.setText(list.get(position).getName());
+        holder.status.setText(list.get(position).getStatus());
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+}
--- app/src/main/java/com/example/firebasesearchtest/MainActivity.java
+++ app/src/main/java/com/example/firebasesearchtest/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.firebasesearchtest;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,6 +14,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -22,12 +24,26 @@ import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+
+import javax.annotation.Nullable;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText etSearchField;
+//    private EditText etSearchField;
+    private SearchView searchView;
     private ImageButton ibSearch;
+    private ArrayList<Users> list;
     private RecyclerView rvResultList;
     private DatabaseReference mUserDatabase;
+    private AdapterClass adapterClass;
+    private FirebaseFirestore firebaseFirestore;
+    private static final String TAG = "FireLog";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,72 +51,122 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mUserDatabase = FirebaseDatabase.getInstance().getReference("Users");
         setupUiViews();
-        ibSearch.setOnClickListener(new View.OnClickListener() {
+//        ibSearch.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                String searchText = etSearchField.getText().toString();
+////                firebaseUserSearch(searchText);
+//            }
+//        });
+        firebaseFirestore.collection("Users").addSnapshotListener(new EventListener<QuerySnapshot>() {
             @Override
-            public void onClick(View v) {
-                String searchText = etSearchField.getText().toString();
-                firebaseUserSearch(searchText);
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+                if (e != null) {
+                    Log.d(TAG, "Error :" + e.getMessage());
+                }
+                for (DocumentChange doc : queryDocumentSnapshots.getDocumentChanges()) {
+                    if (doc.getType() == DocumentChange.Type.ADDED) {
+                        Users users = doc.getDocument().toObject(Users.class);
+                        list.add(users);
+
+                        adapterClass.notifyDataSetChanged();
+                    }
+                }
             }
         });
+        if (searchView != null) {
+            searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String query) {
+                    return false;
+                }
+
+                @Override
+                public boolean onQueryTextChange(String newText) {
+                    search(newText);
+                    return true;
+                }
+            });
+        }
     }
-    private void setupUiViews(){
-        etSearchField = findViewById(R.id.etOne);
+
+    private void search(String str) {
+        ArrayList<Users> myList = new ArrayList<>();
+        for (Users object : list) {
+            if (object.getName().toLowerCase().contains(str.toLowerCase())) {
+                myList.add(object);
+            }
+        }
+        AdapterClass adapterClass = new AdapterClass(myList);
+        rvResultList.setAdapter(adapterClass);
+    }
+
+
+    private void setupUiViews() {
+//        etSearchField = findViewById(R.id.etOne);
         ibSearch = findViewById(R.id.ibSearch);
+        searchView = findViewById(R.id.searchView);
         rvResultList = findViewById(R.id.result_list);
+        list = new ArrayList<>();
+        adapterClass = new AdapterClass(list);
         rvResultList.setHasFixedSize(true);
         rvResultList.setLayoutManager(new LinearLayoutManager(this));
+        rvResultList.setAdapter(adapterClass);
+        firebaseFirestore = FirebaseFirestore.getInstance();
 
     }
 
-    private void firebaseUserSearch(String searchText) {
-        Toast.makeText(MainActivity.this, "Started Search", Toast.LENGTH_SHORT).show();
-        Query query = mUserDatabase.orderByChild("name").startAt(searchText).endAt(searchText + "\uf8ff");
-        FirebaseRecyclerOptions<Users> options =
-                new FirebaseRecyclerOptions.Builder<Users>()
-                        .setQuery(query, Users.class)
-                        .build();
 
-        FirebaseRecyclerAdapter<Users, UsersViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Users, UsersViewHolder>(options) {
-
-            @NonNull
-            @Override
-            public UsersViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_layout, parent ,false);
-
-                return new UsersViewHolder(view);
-            }
-
-            @Override
-            protected void onBindViewHolder(@NonNull UsersViewHolder holder, int position, @NonNull Users model) {
-                holder.setDetails(getApplicationContext(),model.getName(),model.getStatus(),model.getImage());
-            }
-            
-//            protected void populateViewHolder(UsersViewHolder viewHolder, Users model, int position){
-//                viewHolder.setDetails(getApplicationContext(),model.getName(),model.getStatus(),model.getImage());
+//    private void firebaseUserSearch(String searchText) {
+//        Toast.makeText(MainActivity.this, "Started Search", Toast.LENGTH_SHORT).show();
+//        Query query = mUserDatabase.orderByChild("name").startAt(searchText).endAt(searchText + "\uf8ff");
+//        FirebaseRecyclerOptions<Users> options =
+//                new FirebaseRecyclerOptions.Builder<Users>()
+//                        .setQuery(query, Users.class)
+//                        .build();
+//
+//        FirebaseRecyclerAdapter<Users, UsersViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Users, UsersViewHolder>(options) {
+//
+//            @NonNull
+//            @Override
+//            public UsersViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+//                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_layout, parent ,false);
+//
+//                return new UsersViewHolder(view);
 //            }
-
-    };
-        rvResultList.setAdapter(firebaseRecyclerAdapter);
-    }
-
-    // View holder class
-    public static class UsersViewHolder extends RecyclerView.ViewHolder{
-        View mView;
-
-        public UsersViewHolder(@NonNull View itemView) {
-            super(itemView);
-
-            mView = itemView;
-        }
-        public void setDetails(Context ctx, String userName, String userStatus, String userImage){
-            TextView user_name = mView.findViewById(R.id.tvName);
-            TextView user_status = mView.findViewById(R.id.tvStatus);
-            ImageView user_image = mView.findViewById(R.id.ivUsers);
-            user_name.setText(userName);
-            user_status.setText(userStatus);
-
-            Glide.with(ctx).load(userImage).into(user_image);
-        }
-
-    }
+//
+//            @Override
+//            protected void onBindViewHolder(@NonNull UsersViewHolder holder, int position, @NonNull Users model) {
+//                holder.setDetails(getApplicationContext(),model.getName(),model.getStatus(),model.getImage());
+//            }
+//
+////            protected void populateViewHolder(UsersViewHolder viewHolder, Users model, int position){
+////                viewHolder.setDetails(getApplicationContext(),model.getName(),model.getStatus(),model.getImage());
+////            }
+//
+//    };
+//        rvResultList.setAdapter(firebaseRecyclerAdapter);
+//    }
+//
+//    // View holder class
+//    public static class UsersViewHolder extends RecyclerView.ViewHolder{
+//        View mView;
+//
+//        public UsersViewHolder(@NonNull View itemView) {
+//            super(itemView);
+//
+//            mView = itemView;
+//        }
+//        public void setDetails(Context ctx, String userName, String userStatus, String userImage){
+//            TextView user_name = mView.findViewById(R.id.tvName);
+//            TextView user_status = mView.findViewById(R.id.tvStatus);
+//            ImageView user_image = mView.findViewById(R.id.ivUsers);
+//            user_name.setText(userName);
+//            user_status.setText(userStatus);
+//
+//            Glide.with(ctx).load(userImage).into(user_image);
+//        }
+//
+//    }
 }
+
